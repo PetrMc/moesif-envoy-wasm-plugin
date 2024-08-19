adc076e6f7ea5d7c7da1ccaec8edb583b836270a
Petr McAllister <petr.mcallister@solo.io>
2024-08-19 16:45:21 -0700

    Working server
    Signed-off-by: Petr McAllister <petr.mcallister@solo.io>

--- debug/simple-rust-app/src/grpc_service.rs
+++ debug/simple-rust-app/src/grpc_service.rs
@@ -5,7 +5,6 @@ use crate::utils::print_if_debug;
 use tonic::{Request, Response, Status, Streaming};
 use tokio_stream::StreamExt;
 use tokio_stream::wrappers::ReceiverStream;
-use tokio::time::{timeout, Duration};
 
 pub use super::envoy_service_ext_proc_v3;
 pub use crate::envoy_extensions_filters_http_ext_proc_v3::{ProcessingMode, HeaderSendMode, BodySendMode};
@@ -36,37 +35,42 @@ impl envoy_service_ext_proc_v3::external_processor_server::ExternalProcessor for
         mut request: Request<Streaming<envoy_service_ext_proc_v3::ProcessingRequest>>,
     ) -> Result<Response<Self::ProcessStream>, Status> {
 
-        let (tx, rx) = tokio::sync::mpsc::channel(4);
-        let timeout_duration = Duration::from_secs(15); // Set your desired timeout here
+        let (tx, rx) = tokio::sync::mpsc::channel(32);
 
-        // Stream the incoming messages with a timeout
-        while let Ok(Some(message)) = timeout(timeout_duration, request.get_mut().next()).await {
-            match message {
-                Ok(mut msg) => {
-                    // Log incoming headers for debugging
-                    if let Some(envoy_service_ext_proc_v3::processing_request::Request::RequestHeaders(ref headers)) = msg.request {
-                        print_if_debug("Client-to-Server Request Headers", Some(&headers.headers), None);
+        tokio::spawn(async move {
+            // Stream the incoming messages
+            while let Some(message) = request.get_mut().next().await {
+                match message {
+                    Ok(mut msg) => {
+                        // Log incoming headers for debugging
+                        if let Some(envoy_service_ext_proc_v3::processing_request::Request::RequestHeaders(ref headers)) = msg.request {
+                            print_if_debug("Client-to-Server Request Headers", Some(&headers.headers), None);
+                        }
+
+                        if let Some(envoy_service_ext_proc_v3::processing_request::Request::ResponseHeaders(ref headers)) = msg.request {
+                            print_if_debug("Server-to-Client Response Headers", Some(&headers.headers), None);
+                        }
+
+                        // Send the simplified response
+                        let response = simplified_response();
+                        println!("Sending simplified gRPC response: {:?}", response);
+
+                        if let Err(e) = tx.send(Ok(response)).await {
+                            println!("Error sending response: {:?}", e);
+                            break;
+                        }
                     }
-
-                    if let Some(envoy_service_ext_proc_v3::processing_request::Request::ResponseHeaders(ref headers)) = msg.request {
-                        print_if_debug("Server-to-Client Response Headers", Some(&headers.headers), None);
+                    Err(e) => {
+                        println!("Error receiving message: {:?}", e);
+                        if tx.send(Err(Status::internal("Error processing request"))).await.is_err() {
+                            println!("Error sending internal error response: {:?}", e);
+                            break;
+                        }
                     }
-
-                    // Send the simplified response
-                    let response = simplified_response();
-                    println!("Sending simplified gRPC response: {:?}", response);
-
-                    if let Err(e) = tx.send(Ok(response)).await {
-                        println!("Error sending response: {:?}", e);
-                        return Err(Status::internal(format!("Error sending response: {:?}", e)));
-                    }
-                }
-                Err(e) => {
-                    println!("Error receiving message: {:?}", e);
-                    return Err(Status::internal("Error processing request"));
                 }
             }
-        }
+            println!("Stream processing complete.");
+        });
 
         // Return the simplified response stream
         Ok(Response::new(ReceiverStream::new(rx)))
--- debug/simple-rust-app/src/main.rs
+++ debug/simple-rust-app/src/main.rs
@@ -38,11 +38,10 @@ async fn async_main() -> Result<(), Box<dyn std::error::Error>> {
     // Print environment variables if debug is true
     print_if_debug("Startup", None::<&()>, Some(&env_vars));
 
-
     let addr = "0.0.0.0:50051".parse()?;  // Bind to all interfaces, IPv4 and IPv6
     let grpc_service = grpc_service::MoesifGlooExtProcGrpcService::default();
 
-    println!("Starting Moesfi ExtProc gRPC server for Solo.io Gloo Gateway on {}", addr);
+    println!("Starting Moesif ExtProc gRPC server for Solo.io Gloo Gateway on {}", addr);
 
     Server::builder()
         .add_service(grpc_service::envoy_service_ext_proc_v3::external_processor_server::ExternalProcessorServer::new(grpc_service))
